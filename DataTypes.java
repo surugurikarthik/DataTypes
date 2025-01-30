ff9dc7626d348509bfbbc88a7d1a41db18050bca
surugurikarthik <surugoorikarthik@gmail.com>
2025-01-30 09:40:57 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes{
+    public static void main(String[] args){
+        int a =10;
+        System.out.println("Number =" + a);
+        float b= 5.6f;
+        System.out.println("Decimal = " + b);
+        Double c = 1.23445;
+        System.out.println("LongNumber = " + c);
+        Boolean d = false;
+        System.out.println("Boolean = " + d);
+        String str = "Hello, World!";
+        System.out.print(str);
+
+    }
+}
